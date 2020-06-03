78d72b1f15a9168551e04e69aba0b4295a15b12e
jhj5897 <wkdgpwls1@naver.com>
2020-06-03 19:42:40 +0900

    before merge

--- Miraeseat/app/src/main/java/com/capstondesign/miraeseat/TheaterItem.java
+++ Miraeseat/app/src/main/java/com/capstondesign/miraeseat/TheaterItem.java
@@ -13,24 +13,27 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class TheaterItem {
-    private int WIDTH;
-    private int HEIGHT;
+    //private int WIDTH;
+    //private int HEIGHT;
     private final float DENSITY;
 
     private Context ctx;
     private ViewGroup seatplan_layout;
 
+    private TableLayout.LayoutParams btn_params, vertical_space_params, horizontal_space_params;
+
     private TableLayout btnTableLayout;
     private TableRow btnRow;
 
-    private TableLayout.LayoutParams btn_params, vertical_space_params, horizontal_space_params;
+    private Button btn;
 
-    private String[] table_info;
+    private int[] table_info;
     private int[] vertical_criteria, horizontal_criteria;
 
     TheaterItem(Context ctx, ViewGroup viewGroup) {
@@ -40,33 +43,33 @@ public class TheaterItem {
     }
 
     public void init() {
-        table_info = ctx.getResources().getStringArray(R.array.charlotte);
+        table_info = ctx.getResources().getIntArray(R.array.charlotte);
+
+//        int vertical_space =Math.round(Float.parseFloat(table_info[4])*WIDTH);
+//        int horizontal_space =Math.round(Float.parseFloat(table_info[5])*HEIGHT);
 
         btn_params = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
-        btn_params.weight = 1;
+        btn_params.weight = 1f;
 
-        int vertical_space =Math.round(Float.parseFloat(table_info[4])*WIDTH);
-        int horizontal_space =Math.round(Float.parseFloat(table_info[5])*HEIGHT);
-
-        vertical_space_params = new TableLayout.LayoutParams(vertical_space, TableLayout.LayoutParams.WRAP_CONTENT);
-        horizontal_space_params = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, horizontal_space);
+        vertical_space_params = new TableLayout.LayoutParams(convertDpToPx(table_info[4]), TableLayout.LayoutParams.MATCH_PARENT);
+        horizontal_space_params = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, convertDpToPx(table_info[5]));
     }
 
     public void execute() {
         btnTableLayout = CreateTable();
         seatplan_layout.addView(btnTableLayout);
-        CreateRow();
+        btnTableLayout.addView(CreateRow(0, 11, 34));
     }
 
     private TableLayout CreateTable() {
-        int width = Math.round(Float.parseFloat(table_info[0])*WIDTH);
-        int height = Math.round(Float.parseFloat(table_info[1])*HEIGHT);
-        int margin_left = Math.round(Float.parseFloat(table_info[2])*WIDTH);
-        int margin_top = Math.round(Float.parseFloat(table_info[3])*HEIGHT);
-//        int width = convertDpToPx(table_info[0]);
-//        int height = convertDpToPx(table_info[1]);
-//        int margin_left = convertDpToPx(table_info[2]);
-//        int margin_top = convertDpToPx(table_info[3]);
+//        int width = Math.round(Float.parseFloat(table_info[0])*WIDTH);
+//        int height = Math.round(Float.parseFloat(table_info[1])*HEIGHT);
+//        int margin_left = Math.round(Float.parseFloat(table_info[2])*WIDTH);
+//        int margin_top = Math.round(Float.parseFloat(table_info[3])*HEIGHT);
+        int width = convertDpToPx(table_info[0]);
+        int height = convertDpToPx(table_info[1]);
+        int margin_left = convertDpToPx(table_info[2]);
+        int margin_top = convertDpToPx(table_info[3]);
 
         ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(width, height);
         params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
@@ -75,39 +78,53 @@ public class TheaterItem {
 
         TableLayout tableLayout = new TableLayout(ctx);
         tableLayout.setLayoutParams(params);
-        tableLayout.setBackgroundColor(ctx.getResources().getColor(R.color.main_color));
 
         return tableLayout;
     }
 
-    private void CreateRow() {
+    private TableRow CreateRow(int row_index, int start_index, int end_index) {
         btnRow = new TableRow(ctx);
         btnRow.setLayoutParams(btn_params);
+        btnRow.setBackgroundResource(R.drawable.seatbutton);
 
-        CreateButton(11, 14);
-        btnTableLayout.addView(btnRow);
-
-        //왼쪽 패딩 x=8x+(x%4==0) 저 boolean을 어떡할까
-    }
-
-    private void CreateButton(int start_index, int end_index) {
         int n = start_index;
+//        if(n>1) {
+//            int padding = 8*(n-1)+(n/4);
+//            TextView textView = new TextView(ctx);
+//            textView.setWidth(convertDpToPx(padding));
+//            btnRow.addView(textView);
+//        }
 
         for(; n<=end_index; n++) {
-            Button btn = new Button(ctx);
-            btn.setLayoutParams(btn_params);
-            btn.setBackgroundResource(R.drawable.seatbutton);
+            btn = new Button(ctx);
+//            btn.setLayoutParams(btn_params);
+//            btn.setBackgroundResource(R.drawable.seatbutton);
+//            btn.setOnClickListener(SeatButtonOnClickListner);
             btn.setTag(n);
-            btn.setOnClickListener(SeatButtonOnClickListner);
-
             btnRow.addView(btn);
+
+
+            if(n==14 || n==30) {
+                TextView textView = new TextView(ctx);
+                textView.setLayoutParams(vertical_space_params);
+                btnRow.addView(textView);
+            }
         }
+
+//        if(n<44) {
+//            int padding = 8*(44-n)+((44-n)/4);
+//            TextView textView = new TextView(ctx);
+//            textView.setWidth(convertDpToPx(padding));
+//            btnRow.addView(textView);
+//        }
+
+        return btnRow;
     }
 
     public View.OnClickListener SeatButtonOnClickListner = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            String tag = (String) v.getTag();
+            String tag = v.getTag().toString();
             Toast.makeText(ctx, tag, Toast.LENGTH_SHORT).show();
         }
     };
