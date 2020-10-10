d3b1678640efaf43718cab80edd266602e3a129c
mmmmmjjj <myungju25@naver.com>
2020-10-10 20:29:49 +0900

    clear cash, terms upload

---
+++ Miraeseat/app/src/main/java/com/capstondesign/miraeseat/TermsPage.java
@@ -0,0 +1,14 @@
+package com.capstondesign.miraeseat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_page);
+    }
+}
