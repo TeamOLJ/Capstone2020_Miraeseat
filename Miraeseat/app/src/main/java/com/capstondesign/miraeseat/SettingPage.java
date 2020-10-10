c8cd662aa3e70457b0d64edf6babb28a512b5a18
mmmmmjjj <myungju25@naver.com>
2020-10-10 20:30:43 +0900

    clear cash, terms uploaded

--- Miraeseat/app/src/main/java/com/capstondesign/miraeseat/SettingPage.java
+++ Miraeseat/app/src/main/java/com/capstondesign/miraeseat/SettingPage.java
@@ -1,17 +1,23 @@
 package com.capstondesign.miraeseat;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.capstondesign.miraeseat.BuildConfig;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
 
 
 public class SettingPage extends AppCompatActivity {
@@ -24,9 +30,9 @@ public class SettingPage extends AppCompatActivity {
 
     Button btTheme;
     Button btnCash;
+    Button btnTOS;
 
     String versionName = BuildConfig.VERSION_NAME;
-
 
 
     @Override
@@ -46,6 +52,7 @@ public class SettingPage extends AppCompatActivity {
 
         btTheme = (Button) findViewById(R.id.btTheme);
         btnCash = (Button) findViewById(R.id.btnCash);
+        btnTOS = (Button) findViewById(R.id.btnTOS);
 
 
         btTheme.setOnClickListener(new View.OnClickListener() {
@@ -56,19 +63,43 @@ public class SettingPage extends AppCompatActivity {
             }
         });
 
+        btnCash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingPage.this);
+                builder.setTitle("캐시 정리");
+                builder.setMessage(R.string.cash_text);
+                builder.setPositiveButton("지우기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        clearApplicationData(getApplicationContext());
+                        Toast.makeText(getApplicationContext(), "캐시가 지워졌습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("취소", null);
+                builder.show();
+            }
+        });
 
-        versionText = (TextView)findViewById(R.id.verNum);
+        btnTOS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),TermsPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        versionText = (TextView) findViewById(R.id.verNum);
         versionText.setText(versionName);
 
 
     }
 
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 break;
@@ -77,4 +108,34 @@ public class SettingPage extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public static void clearApplicationData(Context context) {
+        File cache = context.getCacheDir();
+        if(cache != null && cache.isDirectory()){
+            deleteDir(cache);
+        }
+//        File appDir = new File(cache.getParent());
+//        if (appDir.exists()) {
+//            String[] children = appDir.list();
+//            for (String s : children) {
+//                if(s.equals("lib") || s.equals("files")) continue;
+//                deleteDir(new File(appDir, s));
+//                Log.d("test", "File /data/data/" + context.getPackageName() + "/" + s + " DELETED");
+//            }
+//        }
+    }
+
+    private static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
+
 }
--- Miraeseat/app/src/main/java/com/capstondesign/miraeseat/TermsPage.java
+++ Miraeseat/app/src/main/java/com/capstondesign/miraeseat/TermsPage.java
@@ -1,14 +1,75 @@
 package com.capstondesign.miraeseat;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TermsPage extends AppCompatActivity {
+
+    FirebaseFirestore db;
+
+    DrawerHandler drawer;
+
+    TextView titleText;
+    TextView TCText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_page);
+        drawer = new DrawerHandler(this);
+        setSupportActionBar(drawer.toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        titleText = (TextView) findViewById(R.id.titleText);
+        titleText.setText("서비스 이용약관");
+
+        db = FirebaseFirestore.getInstance();
+
+        TCText = (TextView)findViewById(R.id.termsText);
+
+        // 데이터베이스에서 약관을 읽어와 텍스트에 설정
+        db.collection("TermsDB").document("TermsDocument").get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        // 파이어베이스는 \n 등의 특수표기문자를 지원하지 않으므로, "\\n"로 표기해둔 부분을 모두 newline 기호로 치환하는 식으로 새 줄 구성
+                        String TC = documentSnapshot.getString("TermCondition").replace("\\\\n", "\n");
+                        TCText.setText(TC);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "약관 정보를 읽어오는 데에 실패했습니다. 인터넷 연결을 확인해주세요.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
